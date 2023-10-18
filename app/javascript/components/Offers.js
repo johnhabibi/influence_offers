a559821b9de743ae5961e87774339cf706ea39a2
John Habibi <johnhabibi@gmail.com>
2023-10-18 00:00:34 -0500

    create react-rails app

---
+++ app/javascript/components/Offers.js
@@ -0,0 +1,12 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+const Offers = (props) => {
+  return (
+    <React.Fragment>
+    </React.Fragment>
+  )
+}
+
+
+export default Offers
